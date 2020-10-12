22e86b6932faa4c63bc11e6c9f8d71b1be2233b8
Jian Mao <jm5159@columbia.edu>
2020-10-12 23:00:26 +0000

    created import_squirrel_data.py

---
+++ sightings/management/commands/import_squirrel_data.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from sightings.models import Squirrel
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('path')
+    def handle(self, *args, **options):
+        with open(options['path']) as fp:
+            reader = csv.DictReader(fp)
+            data = list(reader)
+        squirrels = []
+        for dict_ in data:
+            squirrels.append(Squirrel(
+                latitude=dict_['X'],
+                longtitude=dict_['Y'],
+                squirrel_id=dict_['Unique Squirrel ID'],
+                shift=dict_['Shift'],
+                date=timezone.datetime.strptime(dict_['Date'], '%m%d%Y').date(),
+                age=dict_['Age'],
+                primary_fur_color=dict_['Primary Fur Color'],
+                highlight_fur_color = dict_['Hightlight_Fur_Color'],
+                location = dict_['Location'],
+                specific_location=dict_['Specific Location'],
+                running=True if dict_['Running'].lower() == 'true' else False,
+                chasing=True if dict_['Chasing'].lower() == 'true' else False,
+                climbing=True if dict_['Climbing'].lower() == 'true' else False,
+                eating=True if dict_['Eating'].lower() == 'true' else False,
+                foraging=True if dict_['Foraging'].lower() == 'true' else False,
+                other_activities=dict_['Other Activities'],
+                kuks=True if dict_['Kuks'].lower() == 'true' else False,
+                quaas=True if dict_['Quaas'].lower() == 'true' else False,
+                moans=True if dict_['Moans'].lower() == 'true' else False,
+                tail_flags=True if dict_['Tail flags'].lower() == 'true' else False,
+                tail_twitches=True if dict_['Tail twitches'].lower() == 'true' else False,
+                approaches=True if dict_['Approaches'].lower() == 'true' else False,
+                indifferent=True if dict_['Indifferent'].lower() == 'true' else False,
+                runs_from=True if dict_['Runs from'].lower() == 'true' else False,
+            ))
+        Squirrel.objects.bulk_create(squirrels)
+~                                     
--- sightings/models.py
+++ sightings/models.py
@@ -1,21 +1,45 @@
 from django.db import models
 
 class Squirrel(models.Model):
-    latitude = models.CharField(max_length=100)
-    longtitude = models.CharField(max_length=100)
-    id = models.CharField(max_length=100,primary_key=True)
-    date = models.DateField()
-    shift = models.CharField(max_length=10)
-    age = models.CharField(max_length=3)
-    primary_fur_color = models.CharField(max_length=20)
-    highlight_fur_color = models.CharField(max_length=20)
-    location = models.CharField(max_length=30)
+    latitude = models.FloatField(null=False, blank=False)
+    longtitude = models.FloatField(null=False, blank=False)
+    squirrel_id = models.CharField(max_length=100,unique=True,null=False, blank=False)
+
+    AM = 'AM'
+    PM = 'PM'
+    OTHER = ''
+    shift_choice = [
+            (AM, 'AM'),
+            (PM, 'PM'),
+            (OTHER, '')
+            ]
+    shift = models.CharField(
+            max_length = 15,
+            choices = shift_choice,
+            default = OTHER,
+            )
+
+    date = models.CharField(max_length = 20,blank=True)
+    age = models.CharField(max_length=5,blank=True)
+    primary_fur_color = models.CharField(max_length=20,blank=True)
+    highlight_fur_color = models.CharField(max_length=20,blank=True)
+    location = models.CharField(max_length=50,blank=True)
+    specific_location=models.CharField(max_length=50,  blank=True)
     running = models.BooleanField()
     chasing = models.BooleanField()
     climbing = models.BooleanField()
     eating = models.BooleanField()
     foraging = models.BooleanField()
+    other_activities = models.CharField(max_length=100, blank=True)
+    kuks = models.BooleanField()
+    quaas = models.BooleanField()
+    moans = models.BooleanField()
+    tail_flags = models.BooleanField()
+    tail_twitches = models.BooleanField()
+    approaches = models.BooleanField()
+    indifferent = models.BooleanField()
+    runs_from = models.BooleanField()
 
     def __str__(self):
-        return self.id
+        return self.squirrel_id
 # Create your models here.
