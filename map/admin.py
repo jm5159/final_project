e5fda9c247db301a4ce0936ec2be982f952272d1
Jian Mao <jm5159@columbia.edu>
2020-10-13 02:12:32 +0000

    merge map into sightings

--- map/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Squirrel
-
-admin.site.register(Squirrel)
-# Register your models here.
--- map/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Squirrel
-
-def index(request):
-    return HttpResponse('Hello')
-
-# Create your views here.
