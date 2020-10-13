2f60095c70e6a65af200a1f61866c17b75eff70b
Siyuan Chen <sc4811@columbia.edu>
2020-10-13 00:38:30 +0000

    route change

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail/', views.detail, name='detail'),
-    path('add/', views.add, name='add'),
-    path('stat/', views.stat, name='stat'),
+    path('', views.index, name='index'),    
+    path('sightings/', views.sightings, name='sightings'),
+    path('sightings/detail/', views.detail, name='detail'),
+    path('sightings/add/', views.add, name='add'),
+    path('sightings/stat/', views.stat, name='stat'),
+    path('map/', views.map, name = 'map'),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 
 from .models import Squirrel
 
+def map(request):
+    return HttpResponse('Map')
+
+def sightings(request):
+    return HttpResponse('Sightings')
+
 def index(request):
-    return HttpResponse('Hello')
+    return HttpResponse('Home Index Page')
 
 def detail(request):
-    return HttpResponse('Hello')
+    return HttpResponse('detail')
 
 def add(request):
-    return HttpResponse('Hello')
+    return HttpResponse('add')
 
 def stat(request):
-    return HttpResponse('Hello')
+    return HttpResponse('stat')
 # Create your views here.
