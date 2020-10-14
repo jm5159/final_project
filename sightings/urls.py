13ad58aea1fb7decb430e7b01d3a7d56e9cc455e
Jian Mao <jm5159@columbia.edu>
2020-10-14 22:21:55 +0000

    merge

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,17 +2,14 @@ from django.urls import path
 
 from . import views
 
-from django.urls import path
-
-from . import views
 
 app_name = 'sightings-view'
 
 urlpatterns = [
     path('', views.index, name='index'),    
     path('sightings/', views.sightings, name='sightings'),
-    path('sightings/<str:unique_squirrel_id>/', views.detail, name='detail'),
     path('sightings/add/', views.add, name='add'),
     path('sightings/stats/', views.stats, name='stats'),
+    path('sightings/<str:unique_squirrel_id>/', views.detail, name='detail'),
     path('map/', views.map, name = 'map'),
 ]
