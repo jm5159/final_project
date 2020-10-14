8ae7a0a6b9fb84531abbb52329f56b0b91527555
Siyuan Chen <sc4811@columbia.edu>
2020-10-14 17:21:42 +0000

    updateview

---
+++ sightings/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.db.models import Avg, Max, Min, Count
+from .models import Squirrel
+from .forms import UpdateRequestForm
+
+def map(request):
+    squirrels = Squirrel.objects.all()[:100]
+    context = {'squirrels': squirrels}
+    return render(request, 'sightings/map.html', context)
+
+
+def sightings(request):
+    squirrels = Squirrel.objects.all()
+    context = {'squirrels': squirrels}
+    return render(request, 'sightings/sightings.html', context)
+
+def index(request):
+    squirrels = Squirrel.objects.all()
+    context = {'squirrels': squirrels}
+    return render(request, 'sightings/base.html', context)
+
+def detail(request, unique_squirrel_id):
+    squirrel = Squirrel.objects.get(unique_squirrel_id=unique_squirrel_id)
+    context = {
+        'squirrel': squirrel,
+    }
+    if request.method == "POST":
+        form = UpdateRequestForm(request.POST, instance = squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings/')
+    else:
+        form = UpdateRequestForm(instance = squirrel)
+    return render(request, 'sightings/detail.html', {'form': form, 'unique_squirrel_id':unique_squirrel_id})
+
+    
+    
+
+
+def add(request):
+    return HttpResponse('add')
+
+def request_update(request):
+    if request.method == 'POST':
+        form = UpdateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({})
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
+
+    return JsonResponse({})
+
+def request_add(request):
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({})
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
+
+    return JsonResponse({})
+
+def stats(request):
+    squirrels = Squirrel.objects.all()
+    total = Squirrel.objects.count()
+    avg_lat = squirrels.aggregate(Avg('latitude'))
+    avg_long = squirrels.aggregate(Avg('longitude'))
+    climbing= list(squirrels.values_list('climbing').annotate(Count('climbing')))
+    chasing = list(squirrels.values_list('chasing').annotate(Count('chasing')))
+    age = list(squirrels.values_list('age').annotate(Count('age')))
+    context = {
+            'total':total,
+            'avg_lat':avg_lat,
+            'avg_long':avg_long,
+            'climbing':climbing,
+            'chasing':chasing,
+            'age':age,
+            }
+
+    return render(request, 'sightings/stats.html', context)
+  
+# Create your views here.
+
+
