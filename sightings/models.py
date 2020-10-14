481a25ee5277f1ae0ebdcae070058127016240e3
Jian Mao <jm5159@columbia.edu>
2020-10-14 03:41:01 +0000

    upate import_squirrel_data to remove duplicate id

--- sightings/models.py
+++ sightings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Squirrel(models.Model):
     latitude = models.FloatField(null=False, blank=False)
     longitude = models.FloatField(null=False, blank=False)
-    unique_squirrel_id = models.CharField(max_length=100,null=False, blank=False)
+    unique_squirrel_id = models.CharField(max_length=100,unique=True,null=False, blank=False)
     AM = 'AM'
     PM = 'PM'
     OTHER = ''
